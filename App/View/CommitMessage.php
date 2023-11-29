85e457f29ddd57c01f337615f8306cdd6a572367
MareKubala <151211770+MareKubala@users.noreply.github.com>
2023-11-29 13:39:35 +0100

    Update CommitMessage.php

--- App/View/CommitMessage.php
+++ App/View/CommitMessage.php
@@ -1,1 +1,12 @@
+<?php
 
+namespace App\View;
+
+interface CommitMessage {
+    public function getTitle(): string;
+    public function getTaskId(): ?int;
+    public function getTags(): array;
+    public function getDetails(): array;
+    public function getBCBreaks(): array;
+    public function getTodos(): array;
+}
